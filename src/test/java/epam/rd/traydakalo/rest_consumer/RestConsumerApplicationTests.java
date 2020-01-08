ec75d6daaf541a0254b1cd697109a3c9b97febf7
maxTraydakalo <max.traydakalo@gmail.com>
2020-01-08 12:19:48 +0200

    initial commit

---
+++ src/test/java/epam/rd/traydakalo/rest_consumer/RestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.rd.traydakalo.rest_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
